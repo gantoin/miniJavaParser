3768f9bbce5b34629c46ad46c6aeabbaafd14492
Tim Vahlbrock <tim@vahlbrock.de>
2022-10-31 09:59:28 +0100

    add tests for statement list

--- src/test/java/de/keine_arme_keine_kekse/AppTest.java
+++ src/test/java/de/keine_arme_keine_kekse/AppTest.java
@@ -3,28 +3,26 @@ package de.keine_arme_keine_kekse;
 import java.io.FileNotFoundException;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import de.keine_arme_keine_kekse.parser.MiniJavaParser;
 import de.keine_arme_keine_kekse.parser.ParseException;
-import de.keine_arme_keine_kekse.syntaxtree.Statement;
 
 class AppTest {
 
+    @Disabled
     @ParameterizedTest
     @ValueSource(strings = {
             // "_TEST_Blatt01",
             "_TEST_Blatt02",
-            // "Factorial",
-            // "BubbleSort",
-            // "BinarySearch",
-            // "BinaryTree",
-            // "LinearSearch",
-            // "LinkedList",
-            // "QuickSort",
-            // "TreeVisitor"
+    // "Factorial",
+    // "BubbleSort",
+    // "BinarySearch",
+    // "BinaryTree",
+    // "LinearSearch",
+    // "LinkedList",
+    // "QuickSort",
+    // "TreeVisitor"
     })
     public void recognizesTokens(String sample) throws FileNotFoundException, ParseException {
         App app = new App(sample);
--- src/test/java/de/keine_arme_keine_kekse/MiniJavaParserTest.java
+++ src/test/java/de/keine_arme_keine_kekse/MiniJavaParserTest.java
@@ -11,11 +11,14 @@ import de.keine_arme_keine_kekse.parser.MiniJavaParser;
 import de.keine_arme_keine_kekse.parser.ParseException;
 import de.keine_arme_keine_kekse.syntaxtree.And;
 import de.keine_arme_keine_kekse.syntaxtree.Assign;
+import de.keine_arme_keine_kekse.syntaxtree.BooleanTyping;
 import de.keine_arme_keine_kekse.syntaxtree.Call;
 import de.keine_arme_keine_kekse.syntaxtree.Exp;
 import de.keine_arme_keine_kekse.syntaxtree.ExpList;
 import de.keine_arme_keine_kekse.syntaxtree.False;
 import de.keine_arme_keine_kekse.syntaxtree.IdentifierExp;
+import de.keine_arme_keine_kekse.syntaxtree.IdentifierTyping;
+import de.keine_arme_keine_kekse.syntaxtree.IntArrayTyping;
 import de.keine_arme_keine_kekse.syntaxtree.IntegerLiteral;
 import de.keine_arme_keine_kekse.syntaxtree.IntegerTyping;
 import de.keine_arme_keine_kekse.syntaxtree.LessThan;
@@ -24,10 +27,12 @@ import de.keine_arme_keine_kekse.syntaxtree.Minus;
 import de.keine_arme_keine_kekse.syntaxtree.Not;
 import de.keine_arme_keine_kekse.syntaxtree.Plus;
 import de.keine_arme_keine_kekse.syntaxtree.Statement;
+import de.keine_arme_keine_kekse.syntaxtree.StatementList;
 import de.keine_arme_keine_kekse.syntaxtree.This;
 import de.keine_arme_keine_kekse.syntaxtree.Times;
 import de.keine_arme_keine_kekse.syntaxtree.True;
 import de.keine_arme_keine_kekse.syntaxtree.VarDecl;
+import de.keine_arme_keine_kekse.syntaxtree.VarDeclList;
 
 public class MiniJavaParserTest {
 
@@ -222,6 +227,110 @@ public class MiniJavaParserTest {
         Statement result = parser.AssignStatement();
 
         assertEquals(Assign.class, result.getClass());
+    }
+
+    @Test
+    public void parsesVarDeclListEmpty() throws ParseException {
+        MiniJavaParser parser = parserFor("");
+        VarDeclList result = parser.VarDeclList();
+
+        assertEquals(VarDeclList.class, result.getClass());
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void parsesVarDeclListOne() throws ParseException {
+        MiniJavaParser parser = parserFor("int x;");
+        VarDeclList result = parser.VarDeclList();
+
+        assertEquals(VarDeclList.class, result.getClass());
+
+        assertEquals(1, result.size());
+
+        VarDecl varDecl = result.elementAt(0);
+        assertEquals(IntegerTyping.class, varDecl.type.getClass());
+        assertEquals("x", varDecl.id.getName());
+    }
+
+    @Test
+    public void parsesVarDeclListMany() throws ParseException {
+        MiniJavaParser parser = parserFor("int x; int[] y; boolean z; ObjectXyz a;");
+        VarDeclList result = parser.VarDeclList();
+
+        assertEquals(VarDeclList.class, result.getClass());
+
+        assertEquals(4, result.size());
+
+        VarDecl first = result.elementAt(0);
+        assertEquals("x", first.id.getName());
+        assertEquals(IntegerTyping.class, first.type.getClass());
+
+        VarDecl second = result.elementAt(1);
+        assertEquals("y", second.id.getName());
+        assertEquals(IntArrayTyping.class, second.type.getClass());
+
+        VarDecl third = result.elementAt(2);
+        assertEquals("z", third.id.getName());
+        assertEquals(BooleanTyping.class, third.type.getClass());
+
+        VarDecl fourth = result.elementAt(3);
+        assertEquals("a", fourth.id.getName());
+        assertEquals(IdentifierTyping.class, fourth.type.getClass());
+    }
+
+    @Test
+    public void parsesStatementListEmpty() throws ParseException {
+        MiniJavaParser parser = parserFor("");
+        StatementList result = parser.StatementList();
+
+        assertEquals(StatementList.class, result.getClass());
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void parsesStatementList() throws ParseException {
+        MiniJavaParser parser = parserFor("x = 1; y = 2; z = 3;");
+        StatementList result = parser.StatementList();
+
+        assertEquals(StatementList.class, result.getClass());
+
+        assertEquals(3, result.size());
+
+        Statement first = result.elementAt(0);
+        assertEquals(Assign.class, first.getClass());
+        Assign firstAssign = (Assign) first;
+        assertEquals("x", firstAssign.id.getName());
+        Exp firstExp = firstAssign.exp;
+        assertEquals(IntegerLiteral.class, firstExp.getClass());
+        IntegerLiteral firstLiteral = (IntegerLiteral) firstExp;
+        assertEquals(1, firstLiteral.getValue());
+
+        Statement second = result.elementAt(1);
+        assertEquals(Assign.class, second.getClass());
+        Assign secondAssign = (Assign) second;
+        assertEquals("y", secondAssign.id.getName());
+        Exp secondExp = secondAssign.exp;
+        assertEquals(IntegerLiteral.class, secondExp.getClass());
+        IntegerLiteral secondLiteral = (IntegerLiteral) secondExp;
+        assertEquals(2, secondLiteral.getValue());
+
+        Statement third = result.elementAt(2);
+        assertEquals(Assign.class, third.getClass());
+        Assign thirdAssign = (Assign) third;
+        assertEquals("z", thirdAssign.id.getName());
+        Exp thirdExp = thirdAssign.exp;
+        assertEquals(IntegerLiteral.class, thirdExp.getClass());
+        IntegerLiteral thirdLiteral = (IntegerLiteral) thirdExp;
+        assertEquals(3, thirdLiteral.getValue());
+    }
+
+    @Test
+    public void parsesMethodDeclEmpty() throws ParseException {
+        MiniJavaParser parser = parserFor("public int test(){\nreturn 0;\n}");
+        MethodDecl result = parser.MethodDecl();
+
+        assertEquals(MethodDecl.class, result.getClass());
     }
 
     @Test
